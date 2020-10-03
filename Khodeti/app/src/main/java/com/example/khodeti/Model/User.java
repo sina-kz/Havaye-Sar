e334448a31530c8c70c558beb2c21658ba6ce8a2
alireza <alireza.daghigh1999@gmail.com>
2020-10-03 16:40:08 +0330

    user added

---
+++ Khodeti/app/src/main/java/com/example/khodeti/Model/User.java
@@ -0,0 +1,11 @@
+package com.example.khodeti.Model;
+
+public class User {
+    private String token;
+    private String bitMapByteArray;
+
+    public User(String token, String bitMapByteArray) {
+        this.token = token;
+        this.bitMapByteArray = bitMapByteArray;
+    }
+}
